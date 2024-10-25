ed5a077d4fc3797a53b4716ba5f380b1b88cc563
alansolano1 <alan.solano1@alu.uclm.es>
2024-10-25 22:08:02 +0200

    Facturacion form

---
+++ public/libreria/js/components/cliente-comprar-carro/cliente-comprar-carro-presenter.mjs
@@ -0,0 +1,13 @@
+import { libreriaSession } from "../../commons/libreria-session.mjs";
+import { Presenter } from "../../commons/presenter.mjs";
+import { router } from "../../commons/router.mjs";
+import { model } from "../../model/model.mjs";
+import { ClienteCatalogoLibroPresenter } from "../cliente-catalogo-libro/cliente-catalogo-libro-presenter.mjs";
+import { MensajesPresenter } from "../mensajes/mensajes-presenter.mjs";
+
+export class ClienteComprarCarroPresenter extends Presenter {
+  constructor(model, view) {
+    super(model, view);
+  }
+}
+
--- public/libreria/js/main.mjs
+++ public/libreria/js/main.mjs
@@ -11,6 +11,7 @@ import { InvitadoRegistroPresenter } from "./components/invitado-registro/invita
 import { InvitadoIngresoPresenter } from "./components/invitado-ingreso/invitado-ingreso-presenter.mjs";
 import { ClienteHomePresenter } from "./components/cliente-home/cliente-home-presenter.mjs";
 import { AdminHomePresenter } from "./components/admin-home/admin-home-presenter.mjs";
+import { ClienteComprarCarroPresenter } from "./components/cliente-comprar-carro/cliente-comprar-carro-presenter.mjs";
 
 export function init() {
   seed();
@@ -29,6 +30,7 @@ export function init() {
   router.register(/^\/libreria\/invitado-ver-libro.html/, new InvitadoVerLibroPresenter(model, 'invitado-ver-libro'));
   router.register(/^\/libreria\/admin-ver-libro.html/, new AdminVerLibroPresenter(model, 'admin-ver-libro'));
 
+  router.register(/^\/libreria\/cliente-comprar-carro.html/, new ClienteComprarCarroPresenter(model, 'cliente-comprar-carro'));
 
   router.handleLocation();
 }
