9fdcc2e59bd7140ab67bae3a15aea90e28be564c
ALICIA JIMÉNEZ GONZÁLEZ <Alicia.Jimenez13@alu.uclm.es>
2024-10-30 17:50:14 +0100

    Modificacion cliente-facturas

--- public/libreria/js/components/cliente-facturas/cliente-facturas.mjs
+++ public/libreria/js/components/cliente-facturas/cliente-facturas.mjs
@@ -10,4 +10,63 @@ export class ClienteFacturasPresenter extends Presenter {
       super(model, view);
       this.mensajesPresenter = new MensajesPresenter(model, 'mensajes', '#mensajesContainer');
     }
-}
+    async refresh(){
+      await super.refresh();
+      this.cargarFacturas();
+    }
+
+     cargarFacturas(){
+      const clienteActual=model.getClienteActual();
+
+       if(!clienteActual){
+         alert("No hay clientes autenticados");
+       }
+
+       const facturas = model.getFacturas();
+       const listaCompras= document.getElementById("lista-compras");
+       const totalCompras=document.getElementById("totalcompras");
+
+       listaCompras.innerHTML = '';
+
+       let total = 0;
+
+       facturas.forEach(factura => {
+         if (factura.cliente._id === clienteActual._id) { // Filtra las facturas por el cliente actual
+             const fila = document.createElement("tr");
+
+             // Crear celdas para la fila
+             const celdaNumero = document.createElement("td");
+             celdaNumero.textContent = factura.numero; // Número de la factura
+             fila.appendChild(celdaNumero);
+
+             const celdaFecha = document.createElement("td");
+             celdaFecha.textContent = new Date(factura.fecha).toLocaleDateString(); // Fecha de la factura
+             fila.appendChild(celdaFecha);
+
+             const celdaTotal = document.createElement("td");
+             celdaTotal.textContent = factura.total.toFixed(2) + " €"; // Total de la factura
+             fila.appendChild(celdaTotal);
+
+             const celdaVer = document.createElement("td");
+             const btnVer = document.createElement("button");
+             btnVer.textContent = "Ver";
+             botonVer.onclick = () => {
+              // Redirigir a la página de detalles de la factura
+              window.location.href = 'cliente-ver-compra.html?id=${factura._id}';
+          };
+             fila.appendChild(celdaVer);
+
+             // Añade la fila a la tabla
+             listaCompras.appendChild(fila);
+
+             // Acumula el total
+             total += factura.total;
+           
+         }
+     });
+     document.getElementById("total-final").textContent = totalFinal.toFixed(2);
+
+     }
+ }
+ document.addEventListener("DOMContentLoaded", () => {
+   window.presenter = new ClienteFacturasPresenter(model, "cliente-facturas");})
