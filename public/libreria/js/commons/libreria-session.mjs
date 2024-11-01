b509ac26c5d79d8b7a4af03b993181c3eac18054
alansolano1 <alan.solano1@alu.uclm.es>
2024-11-01 16:02:15 +0100

    Update libreria-session.mjs

--- public/libreria/js/commons/libreria-session.mjs
+++ public/libreria/js/commons/libreria-session.mjs
@@ -2,6 +2,8 @@ import { ROL } from "../model/model.mjs";
 
 const USUARIO_ID = 'USUARIO_ID';
 const USUARIO_ROL = 'USUARIO_ROL';
+const CARRITO = 'CARRITO';
+const CLIENTE_ACTUAL = 'CLIENTE_ACTUAL';
 
 class LibreriaSession {
 
@@ -18,6 +20,7 @@ class LibreriaSession {
   ingreso(usuario) {
     this.setUsuarioId(usuario._id);
     this.setUsuarioRol(usuario.rol);
+    this.setClienteActual(usuario); //Almacenar el cliente actual en la sesion
   }
 
   setUsuarioId(id) { sessionStorage.setItem(USUARIO_ID, id); }
@@ -32,17 +35,54 @@ class LibreriaSession {
   salir() {
     sessionStorage.removeItem(USUARIO_ID);
     sessionStorage.removeItem(USUARIO_ROL);
+    sessionStorage.removeItem(CARRITO);
+    sessionStorage.removeItem(CLIENTE_ACTUAL);
   }
 
   esInvitado() { return !this.getUsuarioRol(); }
   esCliente() { return !this.esInvitado() && this.getUsuarioRol() == ROL.CLIENTE; }
   esAdmin() { return !this.esInvitado() && this.getUsuarioRol() == ROL.ADMIN; }
 
-  formatearMoneda(valor) {
+  agregarAlCarrito(item) {
+    const carrito = this.getCarrito();
+    const itemExistente = carrito.items.find(libro => libro.id === item.id);
 
+    if (itemExistente) {
+      // Si el libro ya está en el carrito, incrementar la cantidad
+      itemExistente.cantidad += item.cantidad;
+    } else {
+      // Si no, agregar el nuevo ítem
+      carrito.items.push(item);
+    }
+
+    this.setCarrito(carrito); // Actualizar el carrito en la sesión
+  };
+
+  formatearMoneda(valor) {
     return this.formatoMoneda.format(valor);
   }
 
+  setCarrito(carrito){
+    sessionStorage.setItem(CARRITO, JSON.stringify(carrito));
+  }
+
+  getCarrito(){
+    const carritoJSON = sessionStorage.getItem(CARRITO);
+    return carritoJSON ? JSON.parse(carritoJSON) : {items:[]};
+  }
+
+  vaciarCarrito(){
+    sessionStorage.removeItem(CARRITO);
+  }
+
+  setClienteActual(cliente){
+    sessionStorage.setItem(CLIENTE_ACTUAL, JSON.stringify(cliente));
+  }
+
+  getClienteActual(){
+    const clienteJSON = sessionStorage.getItem(CLIENTE_ACTUAL);
+    return clienteJSON ? JSON.parse(clienteJSON): null;
+  }
 }
 
 export let libreriaSession = new LibreriaSession();
